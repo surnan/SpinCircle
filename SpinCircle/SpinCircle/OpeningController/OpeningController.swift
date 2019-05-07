c04bc6f9591fb12a81ec1479fffafdee55c4f8b0
Suresh N <forsuresh@yandex.com>
2019-05-07 18:02:15 -0400

    Project successfully starts without using storyboard

---
+++ SpinCircle/SpinCircle/OpeningController/OpeningController.swift
@@ -0,0 +1,17 @@
+//
+//  OpeningController.swift
+//  SpinCircle
+//
+//  Created by admin on 5/7/19.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class OpeningController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
